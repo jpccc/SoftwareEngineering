c062807999a552413a954a3bc511b05a91fc8a11
TSaber7 <741040682@qq.com>
2020-06-02 09:25:43 +0800

    。

--- WebContent/CloseRegistration.jsp
+++ WebContent/CloseRegistration.jsp
@@ -1,12 +1,14 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="ISO-8859-1">
+<meta charset="UTF-8">
 <title>Close Registration</title>
 </head>
 <body>
-	
+	<form action = "CloseRegisServlet" method="get" >
+　　　　<input type="submit" value="submit">
+　　</form>
 </body>
 </html>
---
+++ dao/dao/TSDAO.java
@@ -0,0 +1,49 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TSDAO {
+	public TSDAO() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+	public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/how2java?characterEncoding=UTF-8", "root",
+                "root");
+    }
+	
+	public boolean checkInProgress() {
+		try (Connection c = getConnection(); Statement s = c.createStatement();) {
+			   
+            String sql = "select `status` from Registration where `reg_id`=0";
+   
+            ResultSet rs= s.executeQuery(sql);
+            String result="";
+            if(rs.next()) {
+            	result=rs.getString(1);
+            	System.out.println("result:" + result);
+            }
+            if(result.equals("InProgress")) {
+            		return true;
+            }
+            else if(result.equals("notInProgress")){
+            		return false;
+            }else {
+            	throw new SQLException();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+		return false;
+	}
+}
