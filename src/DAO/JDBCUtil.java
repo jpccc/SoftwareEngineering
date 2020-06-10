ea7c840eec80840242beab26083271ed4e5693a1
TSaber7 <741040682@qq.com>
2020-06-10 20:11:53 +0800

    。

--- src/DAO/JDBCUtil.java
+++ src/DAO/JDBCUtil.java
@@ -5,13 +5,8 @@ import java.sql.*;
 public class JDBCUtil {
     public static Connection getMysqlConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-<<<<<<< HEAD
         return DriverManager.getConnection("jdbc:mysql://localhost:3306/registration?useSSL=false&serverTimezone=UTC",
                 "root", "123456");
-=======
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/course_selection?useSSL=false&serverTimezone=UTC",
-                "root", "010233");
->>>>>>> branch 'master' of https://github.com/jpccc/SoftwareEngineering.git
     }
 
     public static void close(Connection conn, ResultSet rs, PreparedStatement st) throws SQLException {
