de18d890da60d6c841b98faf86ee9a39f46fc468
LiRuiChen <34571459+LiruichenSpace@users.noreply.github.com>
2020-06-10 20:52:51 +0800

    改变修改分数方式

--- src/DAO/JDBCUtil.java
+++ src/DAO/JDBCUtil.java
@@ -5,8 +5,8 @@ import java.sql.*;
 public class JDBCUtil {
     public static Connection getMysqlConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/registration?useSSL=false&serverTimezone=UTC",
-                "root", "123456");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/course_selection?useSSL=false&serverTimezone=UTC",
+                "root", "010233");
     }
 
     public static void close(Connection conn, ResultSet rs, PreparedStatement st) throws SQLException {
